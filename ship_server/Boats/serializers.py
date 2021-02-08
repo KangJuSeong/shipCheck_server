a4c650feff6afbdf2dceefb9c2340791b50ba7c8
강주성 <kjs@kookmin.ac.kr>
2021-02-08 02:07:48 +0000

    불필요한 시리얼라이저 삭제, 필드 변수 변경

--- ship_server/Boats/serializers.py
+++ ship_server/Boats/serializers.py
@@ -6,32 +6,16 @@ class BoatImageSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = BoatImg
-        fields = ('id', 'img', 'lon', 'lat', 'point', 'add_date')
-
-
-class BoatSearchingSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Boat
-        fields = ('id', 'name', 'imo', 'mmsi')
+        fields = '__all__'
 
 
 class BoatSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Boat
-        fields = ('id', 'name', 'imo', 'calsign', 'mmsi', 'vessel_type',
-                  'build_year', 'current_flag', 'home_port', 'main_img',
-                  'is_learning')
-
-
-class ImagesSerializer(serializers.HyperlinkedModelSerializer):
-    images = BoatImageSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Boat
-        fields = ('name', 'images')
+        fields = '__all__'
 
 
 class WasteBoatSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = WasteBoat
-        fields = ('id', 'title', 'latitude', 'longitude', 'detail', 'wasted_img')
+        fields = '__all__'
