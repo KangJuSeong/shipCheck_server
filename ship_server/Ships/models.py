25fd692c670806e0365833d8126594a17ff56a21
강주성 <kjs@kookmin.ac.kr>
2021-02-19 14:27:13 +0900

    DB에 맞게 필드 수정

--- ship_server/Ships/models.py
+++ ship_server/Ships/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from Accounts.models import Account
+import datetime
 
 
 class RegitInfo(models.Model):
@@ -18,15 +19,16 @@ class NormalShip(RegitInfo):
     name = models.CharField(max_length=10, null=True, blank=True)
     port = models.CharField(max_length=10, null=True, blank=True)
     code = models.CharField(max_length=20, null=True, blank=True)
-    tons = models.FloatField(default=0)
+    tons = models.CharField(max_length=10, null=True, blank=True)
     types = models.CharField(max_length=10, null=True, blank=True)
+    size = models.CharField(max_length=15, null=True, blank=True)
     is_vpass = models.BooleanField(default=False)
     is_ais = models.BooleanField(default=False)
     is_vhf = models.BooleanField(default=False)
     is_ff = models.BooleanField(default=False)
     img_cnt = models.IntegerField(default=0)
     is_train = models.BooleanField(default=False)
-    main_img = models.ImageField(upload_to='normal_img/',
+    main_img = models.ImageField(upload_to='normal/new/%Y/%m/%d',
                                  null=True,
                                  blank=True)
 
@@ -34,15 +36,16 @@ class NormalShip(RegitInfo):
         return self.name
     
     def create_normal_ship(data, user):
-        ship = NormalShip.objects.create(name = data['name'],
-                                         port = data['port'],
-                                         code = data['code'],
-                                         tons = data['tons'],
-                                         types = data['types'],
-                                         is_vpass = data['is_vpass'],
-                                         is_ais = data['is_ais'],
-                                         is_vhf = data['is_vhf'],
-                                         is_ff = data['is_ff'],)
+        ship = NormalShip.objects.create(name=data['name'],
+                                         port=data['port'],
+                                         code=data['code'],
+                                         tons=data['tons'],
+                                         types=data['types'],
+                                         width=data['width'],
+                                         is_vpass=data['is_vpass'],
+                                         is_ais=data['is_ais'],
+                                         is_vhf=data['is_vhf'],
+                                         is_ff=data['is_ff'],)
         ship.register = user
         # image = base64.b64decode(data['image_data'])
         # ship.main_img = ContentFile(image_data, str(ship.name)+'.jpg')
@@ -55,7 +58,7 @@ class NormalImage(RegitInfo):
                                on_delete=models.SET_NULL,
                                null=True,
                                blank=True)
-    img = models.ImageField(upload_to='normal_add_img/',
+    img = models.ImageField(upload_to='normal/exist/%Y/%m/%d',
                             null=True,
                             blank=True)
     # lat = models.FloatField(default=0)
@@ -63,13 +66,13 @@ class NormalImage(RegitInfo):
 
 
 class WasteShip(RegitInfo):
-    point = models.TextField(null=True, blank=True)
+    info = models.TextField(null=True, blank=True)
     types = models.CharField(max_length=10, null=True, blank=True)
     lat = models.FloatField(default=0)
     lon = models.FloatField(default=0)
     img_cnt = models.IntegerField(default=0)
     is_train = models.BooleanField(default=False)
-    main_img = models.ImageField(upload_to='waste_img/',
+    main_img = models.ImageField(upload_to='waste/new/%Y/%m/%d',
                                  null=True,
                                  blank=True)
 
@@ -81,6 +84,6 @@ class WasteImage(RegitInfo):
                              on_delete=models.SET_NULL,
                              null=True,
                              blank=True)
-    img = models.ImageField(upload_to='waste_add_img/',
+    img = models.ImageField(upload_to='waste/exist/%Y/%m/%d',
                             null=True,
                             blank=True)
