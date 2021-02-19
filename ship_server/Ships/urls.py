0a6cd3bee8fbcd0c899e9ccdf75127763b9a2945
강주성 <kjs@kookmin.ac.kr>
2021-02-19 14:28:56 +0900

    DB 추가용 API 임시 추가

--- ship_server/Ships/urls.py
+++ ship_server/Ships/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
-from .views import DetailNormalShipAPI, CreateNormalShipAPI
+from .views import DetailNormalShipAPI, CreateNormalShipAPI, NormalShipRegister, AllDelete, WasteShipReigster, NormalImageExtraRegit, WasteImageExtraRegit
 
 urlpatterns = [
     url(r'^normalship/(?P<pk>\d+)/$', DetailNormalShipAPI.as_view()),
     url(r'^normalship/create/', CreateNormalShipAPI.as_view()),
+    url(r'^regitnormal/', NormalShipRegister.as_view()),
+    url(r'^del/', AllDelete.as_view()),
+    url(r'regitwaste/', WasteShipReigster.as_view()),
+    url(r'regitnoramlimg/', NormalImageExtraRegit.as_view()),
+    url(r'regitwasteimg/', WasteImageExtraRegit.as_view()),
 ]
