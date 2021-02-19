6cedec16c1905f5cd1999169a1e2a60d859edee4
강주성 <kjs@kookmin.ac.kr>
2021-02-19 14:26:47 +0900

    ADMIN 필드 수정

--- ship_server/Ships/admin.py
+++ ship_server/Ships/admin.py
@@ -5,29 +5,31 @@ from django.utils.html import format_html
 
 class NormalShipAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'image_tag', 'name', 'port', 'code', 'tons', 'types', 'is_vpass',
-        'is_ais', 'is_vhf', 'is_ff', 'img_cnt', 'is_train', 'register',
-        'regit_date'
+        'id', 'image_tag', 'name', 'port', 'code', 'tons', 'types',
+        # 'is_vpass', 'is_ais', 'is_vhf', 'is_ff', 'is_train',
+        'img_cnt', 'register', 'regit_date',
     )
-    # search_fields = ['name', 'imo']
+    search_fields = ['name']
 
     def image_tag(self, obj):
-        if obj.main_img is None:
-            return ""
-        return format_html('<img src="{}" height="200px;"width="200px;"/>'.format(obj.main_img.url))
+        if obj.main_img == '':
+            img_path = 'http://127.0.0.1:8000/media/NoImage.jpg'
+            return format_html('<img src="{}" height="150px;"width="150px;"/>'.format(img_path))
+        return format_html('<img src="{}" height="150px;"width="150px;"/>'.format(obj.main_img.url))
     image_tag.short_description = 'Image'
 
 
 class WastedShipAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'image_tag', 'point', 'types', 'lat', 'lon', 'is_train',
-        'regit_date', 'register'
+        'id', 'image_tag', 'info', 'types', 'lat', 'lon', 'is_train',
+        'regit_date', 'register', 'img_cnt'
     )
     # search_fields = ['title', 'detail']
 
     def image_tag(self, obj):
-        if obj.main_img is None:
-            return ""
+        if obj.main_img == '':
+            img_path = 'http://127.0.0.1:8000/media/NoImage.jpg'
+            return format_html('<img src="{}" height="150px;"width="150px;"/>'.format(img_path))
         return format_html('<img src="{}" height="200px;"width="200px;"/>'.format(obj.main_img.url))
     image_tag.short_description = 'Image'
 
