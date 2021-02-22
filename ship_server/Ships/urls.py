87ae188559541922ca0c55fd0908589146fb399e
강주성 <kjs@kookmin.ac.kr>
2021-02-23 08:31:46 +0900

    url 수정

--- ship_server/Ships/urls.py
+++ ship_server/Ships/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
 from .views import (DetailNormalShipAPI, CreateNormalShipAPI, ListNormalShipAPI, SearchNormalShipAPI,
-                    DetailWasteShipAPI, CreateWasteShipAPI, ListWasteShipAPI, LocationWasteShipAPI,
-                    )
+                    DetailWasteShipAPI, CreateWasteShipAPI, ListWasteShipAPI, LocationWasteShipAPI,)
+
                     # NormalShipRegister, AllDelete, WasteShipReigster)
 
 urlpatterns = [
     url(r'^ship/normal/(?P<pk>\d+)/$', DetailNormalShipAPI.as_view()),  # 일반 선박 세부 정보
     url(r'^ship/normal/create/', CreateNormalShipAPI.as_view()),  # 일반 선박 추가
-    url(r'^ship/normal/list/', ListNormalShipAPI.as_view()),  # 일반 선박 목록
+    url(r'^ship/normal/list/', ListNormalShipAPI.as_view()),  # 일반 선박 목록  http://127.0.0.1:8000/Ships/ship/normal/list/?page=5
     url(r'^ship/normal/search/', SearchNormalShipAPI.as_view()),  # 일반 선박 검색
     url(r'^ship/waste/(?P<pk>\d+)/$', DetailWasteShipAPI.as_view()),  # 유기,폐 선박 세부 정보
     url(r'^ship/waste/create/', CreateWasteShipAPI.as_view()),  # 유기,폐 선박 추가
