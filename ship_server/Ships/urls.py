a95b0d0453d2287f1e1217c44d144eefdc8848ad
강주성 <kjs@kookmin.ac.kr>
2021-02-22 13:49:05 +0900

    추가된 API url 작성

--- ship_server/Ships/urls.py
+++ ship_server/Ships/urls.py
@@ -1,16 +1,18 @@
 from django.conf.urls import url
 from .views import (DetailNormalShipAPI, CreateNormalShipAPI, ListNormalShipAPI, SearchNormalShipAPI,
-                    DetailWasteShipAPI, CreateWasteShipAPI, ListWasteShipAPI,)
+                    DetailWasteShipAPI, CreateWasteShipAPI, ListWasteShipAPI, LocationWasteShipAPI,
+                    )
                     # NormalShipRegister, AllDelete, WasteShipReigster)
 
 urlpatterns = [
-    url(r'^ship/normal/(?P<pk>\d+)/$', DetailNormalShipAPI.as_view()),
-    url(r'^ship/normal/create/', CreateNormalShipAPI.as_view()),
-    url(r'^ship/normal/list/', ListNormalShipAPI.as_view()),
-    url(r'^ship/normal/search/', SearchNormalShipAPI.as_view()),
-    url(r'^ship/waste/(?P<pk>\d+)/$', DetailWasteShipAPI.as_view()),
-    url(r'^ship/waste/create/', CreateWasteShipAPI.as_view()),
-    url(r'^ship/waste/list/', ListWasteShipAPI.as_view()),
+    url(r'^ship/normal/(?P<pk>\d+)/$', DetailNormalShipAPI.as_view()),  # 일반 선박 세부 정보
+    url(r'^ship/normal/create/', CreateNormalShipAPI.as_view()),  # 일반 선박 추가
+    url(r'^ship/normal/list/', ListNormalShipAPI.as_view()),  # 일반 선박 목록
+    url(r'^ship/normal/search/', SearchNormalShipAPI.as_view()),  # 일반 선박 검색
+    url(r'^ship/waste/(?P<pk>\d+)/$', DetailWasteShipAPI.as_view()),  # 유기,폐 선박 세부 정보
+    url(r'^ship/waste/create/', CreateWasteShipAPI.as_view()),  # 유기,폐 선박 추가
+    url(r'^ship/waste/list/', ListWasteShipAPI.as_view()),  # 유기,폐 선박 목록
+    url(r'^ship/waste/location/', LocationWasteShipAPI.as_view()),  # 유기,폐 선박 좌표 요청
     # url(r'^regitnormal/', NormalShipRegister.as_view()),
     # url(r'^del/', AllDelete.as_view()),
     # url(r'regitwaste/', WasteShipReigster.as_view()),
