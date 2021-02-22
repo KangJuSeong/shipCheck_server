af7b3a15a95e0fb62bffb2054b515515d0647c5c
강주성 <kjs@kookmin.ac.kr>
2021-02-22 12:03:43 +0900

    localhost 추가

--- ship_server/ship_server/settings.py
+++ ship_server/ship_server/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'w$sd)_&da=!kq!w-tbv%3w2m@ih)b689jdosq@atd4i#gi2$ei'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1',
+                 '10.0.2.2']
 
 
 # Application definition
