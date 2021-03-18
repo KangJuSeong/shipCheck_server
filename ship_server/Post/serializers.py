d1e416517872a816bc91b2c7f22dc10b43b1c592
강주성 <kjs@kookmin.ac.kr>
2021-03-18 16:11:25 +0900

    question, answer serializer 작성

--- ship_server/Post/serializers.py
+++ ship_server/Post/serializers.py
@@ -7,3 +7,15 @@ class NoticeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Notice
         fields = ('id', 'title', 'content', 'types', 'date')
+
+
+class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Question
+        fields = ('id', 'title', 'content', 'types', 'date', 'status')
+
+
+class AnswerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Answer
+        fields = ('id', 'date', 'answer')
