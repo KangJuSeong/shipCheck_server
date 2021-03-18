56b850f2f152fbeafff45da94b684b3773cc61e5
강주성 <kjs@kookmin.ac.kr>
2021-03-18 10:42:26 +0900

    Notice, Question, Answer 모델 추가

---
+++ ship_server/Post/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from Accounts.models import Account
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    types = models.CharField(max_length=50)
+    date = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=False)
+    writer = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0}({1}) - {2}".format(self.title, self.id, self.writer)
+
+
+class Answer(models.Model):
+    answer = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    q_id = models.OneToOneField(Question, on_delete=models.CASCADE)
+
+
+class Notice(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    types = models.CharField(max_length=50)
+    date = models.DateTimeField(auto_now_add=True)
---
+++ ship_server/Post/views.py
@@ -0,0 +1,4 @@
+from utils.custom_view import APIView
+from .models import Notice, Question, Answer
+
+
