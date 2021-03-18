2f06b4ccaaa2cf4d056771959ea85a87dea74b81
강주성 <kjs@kookmin.ac.kr>
2021-03-18 16:12:19 +0900

    question, answer API 작성(get메소드)

--- ship_server/Post/urls.py
+++ ship_server/Post/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from .views import NoticeAPI
+from .views import NoticeAPI, QuestionAPI, AnswerAPI
 
 
 urlpatterns = [
-    url(r'^notice/', NoticeAPI.as_view())
+    url(r'^notice/', NoticeAPI.as_view()),
+    url(r'^question/', QuestionAPI.as_view()),
+    url(r'^answer/(?P<pk>\d+)/$', AnswerAPI.as_view())
 ]
