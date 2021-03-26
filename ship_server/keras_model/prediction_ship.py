b107db51beaa5fd61f8322453dbda2976e98c156
강주성 <kjs@kookmin.ac.kr>
2021-03-26 16:24:37 +0900

    save model 파일 불러오기 재작성

--- ship_server/keras_model/prediction_ship.py
+++ ship_server/keras_model/prediction_ship.py
@@ -6,17 +6,14 @@ import os
 
 
 def ai_module(img):
-    start_time = time.time()
-    model = tensorflow.keras.models.load_model(os.getcwd().replace('\\', '/') + '/keras_model/save_model/keras_model.h5'
-                                               , compile=False)
-    print("모델 로딩 시간 : ", time.time() - start_time)
+    model_path = os.listdir(os.getcwd().replace('\\', '/') + '/keras_model/save_model/')
+    model_name = os.getcwd().replace('\\', '/') + '/keras_model/save_model/' + model_path[0]
+    model = tensorflow.keras.models.load_model(model_name, compile=False)
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     size = (224, 224)
     image = img.resize(size)
     image_array = np.asarray(image)
     normalized_image_array = image_array / 255.0
     data[0] = normalized_image_array
-    start_time = time.time()
     prediction = model.predict(data)
-    print("예측 종료 시간 : ", time.time() - start_time)
     return prediction
