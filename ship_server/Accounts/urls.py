b59cccd054aaae6ce0f351adc54218d63f20830b
강주성 <kjs@kookmin.ac.kr>
2021-03-30 18:45:53 +0900

    유저권한 API 작성

--- ship_server/Accounts/urls.py
+++ ship_server/Accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^logout/', LogoutAPI.as_view()),
     url(r'^delete/', DeleteUserAPI.as_view()),
     url(r'^info/', UserInfoAPI.as_view()),
+    url(r'^permission/', UserPermissionAPI.as_view())
 ]
