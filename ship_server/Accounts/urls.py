7f6eabac1046264bce54d0616eb135d8acd37377
KangJuSeong <kjs@kookmin.ac.kr>
2021-05-31 09:31:36 +0900

    code modify

--- ship_server/Accounts/urls.py
+++ ship_server/Accounts/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     url(r'^login/', LoginAPI.as_view()),
     url(r'^signup/', SignUpAPI.as_view()),
     url(r'^logout/', LogoutAPI.as_view()),
-    url(r'^delete/', DeleteUserAPI.as_view()),
     url(r'^info/', UserInfoAPI.as_view()),
     url(r'^permission/', UserPermissionAPI.as_view()),
     url(r'^status/', VersionCheckAPI.as_view()),
